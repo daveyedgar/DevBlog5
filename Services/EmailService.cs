62261fbc1f0b63e5367dadd23c26568c4ce9a9f8
daveyedgar <62836024+daveyedgar@users.noreply.github.com>
2023-06-21 09:05:46 -0400

    updated email settings

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,8 +1,10 @@
 ﻿using DevBlog5.ViewModels;
 using MailKit.Net.Smtp;
 using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
 using MimeKit;
+using System;
 using System.Threading.Tasks;
 
 namespace DevBlog5.Services
@@ -18,23 +20,57 @@ namespace DevBlog5.Services
 
         public async Task SendContactEmailAsync(string name, string emailFrom, string? phone, string subject, string htmlMessage)
         {
-            var email = new MimeMessage();
-            email.Sender = MailboxAddress.Parse(_mailSettings.Email);
-            email.To.Add(MailboxAddress.Parse(_mailSettings.Email));
-            email.Subject = subject;
+
+            var emailSender = _mailSettings.Email ?? Environment.GetEnvironmentVariable("Email");
+
+            MimeMessage newEmail = new();
+
+            newEmail.Sender = MailboxAddress.Parse(emailSender);
+
+            newEmail.To.Add(MailboxAddress.Parse(emailSender));
+
+            newEmail.Subject = subject;
 
             var builder = new BodyBuilder();
+
             builder.HtmlBody = $"<b>{name}</b> has sent you an email and can be reached at: <b>{emailFrom} or {phone}</b><br/><br/>{htmlMessage}";
 
-            email.Body = builder.ToMessageBody();
+            newEmail.Body = builder.ToMessageBody();
 
             using var smtp = new SmtpClient();
-            smtp.Connect(_mailSettings.EmailHost, _mailSettings.EmailPort, SecureSocketOptions.StartTls);
-            smtp.Authenticate(_mailSettings.Email, _mailSettings.EmailPassword);
 
-            await smtp.SendAsync(email);
+            var host = _mailSettings.EmailHost ?? Environment.GetEnvironmentVariable("EmailHost");
+            var port = _mailSettings.EmailPort != 0 ? _mailSettings.EmailPort : int.Parse(Environment.GetEnvironmentVariable("EmailPort")!);
+            var password = _mailSettings.EmailPassword ?? Environment.GetEnvironmentVariable("EmailPassword");
+
+            smtp.Connect(host, port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(emailSender, password);
+
+            await smtp.SendAsync(newEmail);
 
             smtp.Disconnect(true);
+
+
+
+            //var email = new MimeMessage();
+
+            //email.Sender = MailboxAddress.Parse(_mailSettings.Email);
+            //email.To.Add(MailboxAddress.Parse(_mailSettings.Email));
+            //email.Subject = subject;
+
+            //var builder = new BodyBuilder();
+            //builder.HtmlBody = $"<b>{name}</b> has sent you an email and can be reached at: <b>{emailFrom} or {phone}</b><br/><br/>{htmlMessage}";
+
+            //email.Body = builder.ToMessageBody();
+
+            //using var smtp = new SmtpClient();
+
+            //smtp.Connect(_mailSettings.EmailHost, _mailSettings.EmailPort, SecureSocketOptions.StartTls);
+            //smtp.Authenticate(_mailSettings.Email, _mailSettings.EmailPassword);
+
+            //await smtp.SendAsync(email);
+
+            //smtp.Disconnect(true);
         }
 
         public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
