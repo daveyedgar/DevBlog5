2eceddbc6784e1bab5f156812cd2624147aab33b
daveyedgar <SUNDAY@SLEEPYCAFE.COM>
2023-10-09 19:01:03 -0400

    optimization begins

--- wwwroot/assets/js/custom.js
+++ wwwroot/assets/js/custom.js
@@ -118,11 +118,11 @@ wow.init();
 
 
 // preloader
-jQuery(window).load(function() { // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(100).css({
-        'overflow': 'visible'
-    });
-})
+//jQuery(window).load(function() { // makes sure the whole site is loaded
+//    $('#status').fadeOut(); // will first fade out the loading animation
+//    $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+//    $('body').delay(100).css({
+//        'overflow': 'visible'
+//    });
+//})
 
--- wwwroot/assets/js/site.js
+++ wwwroot/assets/js/site.js
@@ -56,119 +56,119 @@ $(document).ready(function () {
 //              COOKIE MODAL
 //
 // **************************************************
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+//function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-// Setup GTM dataLayer -- load
-window.dataLayer = window.dataLayer || [];
+//// Setup GTM dataLayer -- load
+//window.dataLayer = window.dataLayer || [];
 
-// see if cookie exists, if not show cookiebar -- recommended to do this in head of page
-if (!getCookie("CookieConsent")) {
+//// see if cookie exists, if not show cookiebar -- recommended to do this in head of page
+//if (!getCookie("CookieConsent")) {
 
-    document.getElementsByClassName("js-cookiebar")[0].style.display = "block";
+//    document.getElementsByClassName("js-cookiebar")[0].style.display = "block";
 
-    window.dataLayer = [{
-        'event': 'NoCookieExist'
-    }];
-} else {
-    var cookieValue =
-        "{" + '"cookieValue"' + ":" + getCookie("CookieConsent") + "}";
-    var jsonData = JSON.parse(cookieValue);
-    pushCookieValueToDataLayer(jsonData.cookieValue, "CookieBarStatus", false);
-}
+//    window.dataLayer = [{
+//        'event': 'NoCookieExist'
+//    }];
+//} else {
+//    var cookieValue =
+//        "{" + '"cookieValue"' + ":" + getCookie("CookieConsent") + "}";
+//    var jsonData = JSON.parse(cookieValue);
+//    pushCookieValueToDataLayer(jsonData.cookieValue, "CookieBarStatus", false);
+//}
 
-// get Cookie by name - credits to stackoverflow
-function getCookie(cname) {
-    var name = cname + "=",
-        decodedCookie = decodeURIComponent(document.cookie),
-        ca = decodedCookie.split(";");
+//// get Cookie by name - credits to stackoverflow
+//function getCookie(cname) {
+//    var name = cname + "=",
+//        decodedCookie = decodeURIComponent(document.cookie),
+//        ca = decodedCookie.split(";");
 
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == " ") {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
+//    for (var i = 0; i < ca.length; i++) {
+//        var c = ca[i];
+//        while (c.charAt(0) == " ") {
+//            c = c.substring(1);
+//        }
+//        if (c.indexOf(name) == 0) {
+//            return c.substring(name.length, c.length);
+//        }
+//    }
+//    return "";
+//}
 
-// push cookieValue to dataLayer - set array, set eventName
-function pushCookieValueToDataLayer(arr, eventName, boolean) {
-    var x = arr.filter(function (v) {
-        return v.value === true;
-    });
-    dataLayerType(x, eventName, boolean);
-}
+//// push cookieValue to dataLayer - set array, set eventName
+//function pushCookieValueToDataLayer(arr, eventName, boolean) {
+//    var x = arr.filter(function (v) {
+//        return v.value === true;
+//    });
+//    dataLayerType(x, eventName, boolean);
+//}
 
-function dataLayerType(x, e, b) {
-    if (b) {
-        for (var i = 0; i < x.length; i++) {
-            window.dataLayer.push(_defineProperty({}, x[i].name, x[i].value));
-        }
-        window.dataLayer.push({
-            'event': e
-        });
-    } else {
-        var dataInit = [];
-        for (var i = 0; i < x.length; i++) {
-            dataInit.push(_defineProperty({}, x[i].name, x[i].value));
-        };
-        dataInit.push({
-            'event': e
-        });
-        window.dataLayer = dataInit;
-    }
-}
+//function dataLayerType(x, e, b) {
+//    if (b) {
+//        for (var i = 0; i < x.length; i++) {
+//            window.dataLayer.push(_defineProperty({}, x[i].name, x[i].value));
+//        }
+//        window.dataLayer.push({
+//            'event': e
+//        });
+//    } else {
+//        var dataInit = [];
+//        for (var i = 0; i < x.length; i++) {
+//            dataInit.push(_defineProperty({}, x[i].name, x[i].value));
+//        };
+//        dataInit.push({
+//            'event': e
+//        });
+//        window.dataLayer = dataInit;
+//    }
+//}
 
-// set cookie on click this is the accept button in the modal
-function onCookieButtonClick() {
-    var basicValue = document.getElementById("basic_chkbx").checked,
-        preferencesValue = document.getElementById("preferences_chkbx").checked,
-        statisticsValue = document.getElementById("statistics_chkbx").checked,
-        marketingValue = document.getElementById("marketing_chkbx").checked,
-        options = [
-            { name: "basic", value: basicValue },
-            { name: "preferences", value: preferencesValue },
-            { name: "statistics", value: statisticsValue },
-            { name: "marketing", value: marketingValue }
-        ],
-        data = JSON.stringify(options),
-        date = new Date();
+//// set cookie on click this is the accept button in the modal
+//function onCookieButtonClick() {
+//    var basicValue = document.getElementById("basic_chkbx").checked,
+//        preferencesValue = document.getElementById("preferences_chkbx").checked,
+//        statisticsValue = document.getElementById("statistics_chkbx").checked,
+//        marketingValue = document.getElementById("marketing_chkbx").checked,
+//        options = [
+//            { name: "basic", value: basicValue },
+//            { name: "preferences", value: preferencesValue },
+//            { name: "statistics", value: statisticsValue },
+//            { name: "marketing", value: marketingValue }
+//        ],
+//        data = JSON.stringify(options),
+//        date = new Date();
 
-    date.setTime(date.getTime() + 3650 * 24 * 60 * 60 * 1000);
-    var expires = "expires=" + date.toUTCString();
+//    date.setTime(date.getTime() + 3650 * 24 * 60 * 60 * 1000);
+//    var expires = "expires=" + date.toUTCString();
 
-    // set cookie
-    document.cookie = "CookieConsent=" + data + ";" + expires + ";";
-    pushCookieValueToDataLayer(options, "CookiePreferencesChange", true);
+//    // set cookie
+//    document.cookie = "CookieConsent=" + data + ";" + expires + ";";
+//    pushCookieValueToDataLayer(options, "CookiePreferencesChange", true);
 
-    document.getElementsByClassName("js-cookiebar")[0].style.display = "none";
-}
+//    document.getElementsByClassName("js-cookiebar")[0].style.display = "none";
+//}
 
-// show cookiebar on click
-var changeCookie = document.getElementById("open-cookie");
+//// show cookiebar on click
+//var changeCookie = document.getElementById("open-cookie");
 
-// Get cookievalue, set checkboxes to right value and show cookiebar
-changeCookie.addEventListener(
-    "click",
-    function () {
-        if (getCookie("CookieConsent")) {
-            var cookieValue =
-                "{" + '"cookieValue"' + ":" + getCookie("CookieConsent") + "}";
-            var jsonData = JSON.parse(cookieValue);
-            var x = jsonData.cookieValue.filter(function (v) {
-                return v.value === true;
-            });
+//// Get cookievalue, set checkboxes to right value and show cookiebar
+//changeCookie.addEventListener(
+//    "click",
+//    function () {
+//        if (getCookie("CookieConsent")) {
+//            var cookieValue =
+//                "{" + '"cookieValue"' + ":" + getCookie("CookieConsent") + "}";
+//            var jsonData = JSON.parse(cookieValue);
+//            var x = jsonData.cookieValue.filter(function (v) {
+//                return v.value === true;
+//            });
 
-            for (let i = 0; i < x.length; i++) {
-                let y = x[i].name + "_chkbx";
-                document.getElementById(y).checked = true;
-            }
-        }
+//            for (let i = 0; i < x.length; i++) {
+//                let y = x[i].name + "_chkbx";
+//                document.getElementById(y).checked = true;
+//            }
+//        }
 
-        document.getElementsByClassName("js-cookiebar")[0].style.display = "block";
-    },
-    false
-);
+//        document.getElementsByClassName("js-cookiebar")[0].style.display = "block";
+//    },
+//    false
+//);
